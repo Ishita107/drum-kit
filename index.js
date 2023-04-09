087ad015edc53ba218555323c617abee076a3cd5
Ishita <gargishita2874@gmail.com>
2023-04-09 16:45:38 +0530

    adding event listener to all buttons

---
+++ index.js
@@ -0,0 +1,5 @@
+for(var i =0; i<document.querySelectorAll(".drum").length ; i++){   // adding event listeners to all buttons through for loop
+    document.querySelectorAll(".drum")[i].addEventListener("click" , function(){
+          // STORING the inner html (text) of the element. // this returns identity of elements.
+    })
+}
