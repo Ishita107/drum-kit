8e106df2efb60e09d9afa4590252789f1d9af317
Ishita <gargishita2874@gmail.com>
2023-04-09 17:02:36 +0530

    adding sounds to buttons using javascript objects and  switch case

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 for(var i =0; i<document.querySelectorAll(".drum").length ; i++){   // adding event listeners to all buttons through for loop
     document.querySelectorAll(".drum")[i].addEventListener("click" , function(){
           var buttonInnerHtml = this.innerHTML;// STORING the inner html (text) of the element. // this returns identity of elements.
-        switch (buttonInnerHtml) {
+        switch (buttonInnerHtml) {          // ADDING SOUNDS USING SWITCH CASE;
             case "w" :
-            var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
-            break;
+                var tom1 = new Audio("sounds/tom-1.mp3");  // Audio is constructor function
+                tom1.play();
+                break; 
 
-            case "a" :
-            var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();
-            break;
+            case "a" :                                      //  tom1 ,tom2,crash...... are objects
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
             
             case "s" :
-            var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
-            break;
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
             
             case "d" :
-            var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
-            break;
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
             
             case "j" :
-            var crash = new Audio("sounds/crash.mp3");
-            crash.play();
-            break;
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;    
             
             case "k" :
-            var kick = new Audio("sounds/kick.mp3");
-            kick.play();
-            break;
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
 
             case "l" :
-            var snare = new Audio("sounds/snare.mp3");
-            snare.play();
-            break;
+                var kick = new Audio("sounds/kick.mp3");
+                kick.play();
+                break;
 
             default :
-            alert("wrong button or key pressed");
+                alert("wrong button or key pressed");
         }
     })
 }
